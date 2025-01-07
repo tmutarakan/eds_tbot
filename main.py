881e6acf5159029cef91861bdaa8e4cd8a2d8ed4
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2025-01-07 12:49:57 +0300

    add workflow

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+from pprint import pprint
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import Message, User
 from aiogram_dialog import Dialog, DialogManager, StartMode, Window, setup_dialogs
-from aiogram_dialog.widgets.text import Const  # Здесь будем импортировать нужные виджеты
+from aiogram_dialog.widgets.text import Format
 from environs import Env
+
 
 env = Env()
 env.read_env()
 
 BOT_TOKEN = env('BOT_TOKEN')
 
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
@@ -21,28 +21,26 @@ class StartSG(StatesGroup):
     start = State()
 
 
-async def some_handler(callback: CallbackQuery, dialog_manager: DialogManager):  # Здесь будут хэндлеры
-    pass
+async def username_getter(**kwargs):
+    pprint(kwargs)
+    return {'username': kwargs['event_chat'].first_name}
 
 
-async def some_getter(**kwargs):  # Здесь будем создавать нужные геттеры
-    pass
-
-
-# Это стартовый диалог
 start_dialog = Dialog(
     Window(
-        ...  # Здесь будем добавлять виджеты и геттеры
+        Format('Привет, {username}!'),
+        getter=username_getter,
+        state=StartSG.start
     ),
 )
 
 
-# Этот классический хэндлер будет срабатывать на команду /start
 @dp.message(CommandStart())
 async def command_start_process(message: Message, dialog_manager: DialogManager):
     await dialog_manager.start(state=StartSG.start, mode=StartMode.RESET_STACK)
 
 
-dp.include_router(start_dialog)
-setup_dialogs(dp)
-dp.run_polling(bot)
+if __name__ == '__main__':
+    dp.include_router(start_dialog)
+    setup_dialogs(dp)
+    dp.run_polling(bot)
